5b6569970cac5efba29f6463e559626bf33e96c4
George Attaloglou <georgeeattaloglou@gmail.com>
2026-07-19 15:50:47 +0300

    add image and link extraction

--- src/extract_images_and_links.py
+++ src/extract_images_and_links.py
@@ -1,7 +1,9 @@
 import re
 
-def extract_markdown_images(text:str):
-    pass
+def extract_markdown_images(text:str) -> list:
+    matches = re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
 
-def extract_markdown_links(text:str):
-    pass
+def extract_markdown_links(text:str) -> list:
+    matches = re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+    return matches
